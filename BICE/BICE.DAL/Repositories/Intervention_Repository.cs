604a56f1858a8e06b8d3f1a57837326599db0424
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-04-24 20:08:46 +0200

    Intervention API

--- BICE/BICE.DAL/Repositories/Intervention_Repository.cs
+++ BICE/BICE.DAL/Repositories/Intervention_Repository.cs
@@ -9,29 +9,122 @@ namespace BICE.DAL
 {
     public class Intervention_Repository : Repository<Intervention_DAL>
     {
-        public override Intervention_DAL GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override Intervention_DAL Update(Intervention_DAL entity)
-        {
-            throw new NotImplementedException();
-        }
-
         public override IEnumerable<Intervention_DAL> GetAll()
         {
-            throw new NotImplementedException();
+            var query = "SELECT * FROM Interventions";
+
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                
+                using (var command = new SqlCommand(query, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            yield return new Intervention_DAL(
+                                (int)reader["id"],
+                                (string)reader["denomination"],
+                                (string)reader["description"],
+                                (DateTime)reader["startDate"],
+                                (DateTime)reader["endDate"]
+                            );
+                        }
+                    }
+                }
+            }
         }
 
+        public override Intervention_DAL GetById(int id)
+        {
+            var query = "SELECT * FROM Vehicles WHERE id = @id";
+            
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new Intervention_DAL(
+                                (int)reader["id"],
+                                (string)reader["denomination"],
+                                (string)reader["description"],
+                                (DateTime)reader["startDate"],
+                                (DateTime)reader["endDate"]
+                            );
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        public override Intervention_DAL Update(Intervention_DAL intervention)
+        {
+            var query = "UPDATE Interventions SET denomination = @denomination, description = @description, startDate = @startDate, endDate = @endDate WHERE id = @id";
+            
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@id", intervention.Id);
+                    command.Parameters.AddWithValue("@denomination", intervention.Denomination);
+                    command.Parameters.AddWithValue("@description", intervention.Description);
+                    command.Parameters.AddWithValue("@startDate", intervention.StartDate);
+                    command.Parameters.AddWithValue("@endDate", intervention.EndDate);
+                    
+                    command.ExecuteNonQuery();
+                }
+            }
+            return intervention;
+        }
+
+        
         public override Intervention_DAL Insert(Intervention_DAL intervention)
         {
-            throw new NotImplementedException();
+            var query = "INSERT INTO Interventions (denomination, description, startDate, endDate) VALUES (@denomination, @description, @startDate, @endDate)";
+            
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@denomination", intervention.Denomination);
+                    command.Parameters.AddWithValue("@description", intervention.Description);
+                    command.Parameters.AddWithValue("@startDate", intervention.StartDate);
+                    command.Parameters.AddWithValue("@endDate", intervention.EndDate);
+                    
+                    command.ExecuteNonQuery();
+                }
+            }
+            return intervention;
         }
 
         public override void Delete(Intervention_DAL intervention)
         {
-            throw new NotImplementedException();
+            var query = "DELETE FROM Interventions WHERE id = @id";
+            
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@id", intervention.Id);
+                    
+                    command.ExecuteNonQuery();
+                }
+            }
         }
         
     }
