2076a10ace008d7fabd8aa81ddaa05e150d70203
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-04-24 20:35:53 +0200

    DTO

--- BICE/BICE.SRV/Material_SRV.cs
+++ BICE/BICE.SRV/Material_SRV.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using BICE.DAL;
+using BICE.DTO;
+using BICE.BLL;
 
 namespace BICE.SRV
 {
@@ -10,6 +12,33 @@ namespace BICE.SRV
 		{
 			_materialRepository = new Material_Repository();
 		}
+		
+public IEnumerable<Material_DTO> GetMaterial()
+		{
+			IEnumerable<Material_DAL> materialDal = _materialRepository.GetAll();
+			List<Material_DTO> materialDto = new List<Material_DTO>();
+			foreach (Material_DAL material in materialDal)
+			{
+				materialDto.Add(new Material_DTO(material));
+			}
+			return materialDto;
+		}
+
+		public Material_DTO GetMaterialById(int id)
+		{
+			Material_DAL materialDal = _materialRepository.GetById(id);
+			return new Material_DTO(materialDal);
+		}
+		
+		public Material_DTO AddMaterial(Material_DTO materialDto)
+		{
+			Material_BLL materialBll = materialDto.ToBLL();
+			Material_DAL materialDal = new Material_DAL(materialBll);
+			Material_DAL insertedMaterial = _materialRepository.Insert(materialDal);
+			return new Material_DTO(insertedMaterial);
+		}
+		
+	
 		
 		
 	}
